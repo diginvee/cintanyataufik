c21310516b9adfb669be97dd60afcccee7866f9f
Reihan Faizal Hanif <mk.reihan.hanif@pertamina.com>
2024-06-25 15:13:55 +0700

    update

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/cintanyataufik/', // Replace with your repository name
+  build: {
+    outDir: 'dist', // Ensure the build output directory is correct
+  },
+});
